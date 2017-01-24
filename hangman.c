b10a9449e8743c7f900e5c64c2f8f468f67b7616
Hannes Frank <hannes.frank@online.de>
2017-01-24 20:50:05 +0100

    Removed trailing whitespace.

--- hangman.c
+++ hangman.c
@@ -9,13 +9,13 @@ struct game_state
 {
 	// zu ratendes Wort
 	char *to_guess;
-	
+
 	// sichtbarer Teil des Wortes
 	char *visible;
-	
+
 	// alle schon falsch geratenen Buchstaben
 	char *wrong;
-	
+
 	// wie weit der Galgen aufgebaut ist
 	unsigned int progress;
 };
@@ -155,16 +155,16 @@ int main()
 {
 	struct game_state state;
 	char input[1024];
-	
+
 	state = init();
-	
+
 	while (1)
 	{
 		print_state(&state);
-		
+
 		if (game_over(&state))
 			break;
-		
+
 		fgets(input, 1024, stdin);
 		update_state(&state, input);
 	}
